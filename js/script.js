45807816a87a4b644a42323bac1887e734c98ede
indrangrha <indran739@gmail.com>
2024-08-24 18:54:23 +0700

    membuat search form

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
-//klik hamburger menu diklik
 const hamburgerMenu = document.querySelector("#hamburger-menu");
-
 const navbarNav = document.querySelector(".navbar-nav");
+const searchButton = document.querySelector("#search-button");
 
+// Toggle class active untuk hamburger menu
 hamburgerMenu.addEventListener("click", function () {
   navbarNav.classList.toggle("active");
 });
 
-//Klik diluar sidebar untuk menghilangkan nav
+// Toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+//Klik diluar elemen
 document.addEventListener("click", function (e) {
   if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
